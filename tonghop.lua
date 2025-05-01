147ae8c0e4303e9e053251df445732268cbbaac3
Khai456 <leduchoangkhai@gmail.com>
2025-05-01 19:02:08 +0700

    Update tonghop.lua

--- tonghop.lua
+++ tonghop.lua
@@ -21,7 +21,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/Khai456/menu/refs/he
   })
 
        MinimizeButton({
-       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Image = "http://www.roblox.com/asset/?id=123891326268476",
        Size = {60, 60},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
