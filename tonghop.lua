1d128994d5df81fa1bac72191e76fbf1d4e04c0e
Khai456 <leduchoangkhai@gmail.com>
2025-05-01 17:22:05 +0700

    Add files via upload

---
+++ tonghop.lua
@@ -0,0 +1,62 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Khai456/menu/refs/heads/main/menu.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "KHAIDEPZAI",
+         Animation = "KHAIDEPZAI"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=83190276951914",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub",
+    Callback = function()
+	  local Settings = {
+JoinTeam = "Pirates"; -- Pirates/Marines
+Translator = true; -- true/false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+  end
+  })
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "trial",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/giaotrinhhoc/ZisRb/refs/heads/main/ZisRbV5"))()
+  end
+  })
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "fullmoon",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Khai456/fullmn/refs/heads/main/fullmn.lua"))()
+  end
+  })
