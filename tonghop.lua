c473ba4fc1da0f5ef5f1be8fe7a2c14a9046bb8e
Khai456 <leduchoangkhai@gmail.com>
2025-05-02 11:30:44 +0700

    Update tonghop.lua

--- tonghop.lua
+++ tonghop.lua
@@ -60,3 +60,13 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/giaotrinhhoc/ZisRb/re
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Khai456/fullmn/refs/heads/main/fullmn.lua"))()
   end
   })
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "banana hub",
+    Callback = function()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "OHCHIKfYVmVMdEWeGFUtdwzRGxdmwEWQ"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+  end
+  })
