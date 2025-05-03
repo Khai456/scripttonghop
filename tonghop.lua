be284943f96c0ce5ff379d3f4963e6f4a0623c9b
Khai456 <leduchoangkhai@gmail.com>
2025-05-03 13:17:06 +0700

    Update tonghop.lua

--- tonghop.lua
+++ tonghop.lua
@@ -60,3 +60,14 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/giaotrinhhoc/ZisRb/re
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Khai456/fullmn/refs/heads/main/fullmn.lua"))()
   end
   })
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "hop boss",
+    Callback = function()
+getgenv().Mode = "Dough King" -- rip_indra, Dough King, Pull Lever, TOTS, Darkbeard
+getgenv().Team = "Marines"
+repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f0952c4a5f3db9e01f4dbd099c1f9b3c.lua"))()"))()
+  end
+  })
